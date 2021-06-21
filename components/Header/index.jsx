a05abf5ee5296c9d516fcc25eb8eddb170906bb8
alex_hcj <alexhcj@yandex.ru>
2021-06-21 18:23:19 +0300

    refactor folder structure

--- components/Header/index.jsx
+++
@@ -1,5 +0,0 @@
-import s from "./header.module.css";
-
-export const Header = () => {
-  return <h1 className={s.h1}>Header component</h1>;
-};
--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import { Home, Shop, Blog, Cart, Checkout, Product } from './pages/Home'
+import { Header } from './components/Header'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-eto tochno YA sdelal
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<Header />
+			<Switch>
+				<Route path='/'>
+					<Home />
+				</Route>
+				<Route path='/shop'>
+					<Shop />
+				</Route>
+				<Route path='/blog'>
+					<Blog />
+				</Route>
+				<Route path='/cart'>
+					<Cart />
+				</Route>
+				<Route path='/checkout'>
+					<Checkout />
+				</Route>
+				<Route path='/product'>
+					<Product />
+				</Route>
+			</Switch>
+		</div>
+	)
 }
 
-export default App;
+export default App
