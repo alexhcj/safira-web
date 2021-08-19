aec2ab6707f59749fafa11deafa8acadb8cd0d43
alex_hcj <alexhcj@yandex.ru>
2021-08-19 11:18:23 +0300

    refactor shop

--- src/components/ShopComp/ShopSideBar/index.jsx
+++ src/components/ShopComp/ShopSideBar/index.jsx
@@ -1,38 +1,49 @@
 import { useEffect, useState } from 'react'
-import s from './ShopSideBar.module.css'
-import { productsAPI } from '../../../api'
+import { shopProductsAPI } from '../../../api'
 import { convertArray } from '../../../utils'
+import s from './shopsidebar.module.css'
 
 export const ShopSideBar = () => {
-    const [featuredProducts, setFeaturedProducts] = useState([])
-    useEffect(() => {
+	const [shopProducts, setShopProducts] = useState([])
+	const [input, setInput] = useState('')
+
+	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const data = await productsAPI.getProducts()
-				setFeaturedProducts(convertArray(data, 3))
+				const data = await shopProductsAPI.getShopProducts(input)
+				setShopProducts(convertArray(data, 3))
 			} catch (e) {
 				console.log(e)
 			}
 		}
 
 		fetchData()
-	}, [])
-    function  searchByName(event) {
-        let nameSearch = event.target.value
-        
-        
+	}, [input])
 
-        if(nameSearch.length<=3){
-            let z
-        } else {
-            let x
-        }
+	const searchByName = (event) => {
+		setInput(event.target.value)
+	}
 
-    }
+	const debounce = (fn, ms) => {
+		let timeout
 
-    return (
-    <section>
-        <input type="text" onInput={searchByName}/>
-    </section>
-    )
+		return function () {
+			const fnCall = () => {
+				fn.apply(this, arguments)
+			}
+
+			clearTimeout(timeout)
+
+			timeout = setTimeout(fnCall, ms)
+		}
+	}
+
+	const search = debounce(searchByName, 500)
+    
+	return (
+		<section>
+			<input type='text' onChange={search} />
+			{/* {console.log(shopProducts[0][1].name)} */}
+		</section>
+	)
 }
