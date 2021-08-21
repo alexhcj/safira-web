e7bc41f6141e2eefcb2374690f02844072e67895
alex_hcj <alexhcj@yandex.ru>
2021-08-21 12:09:54 +0300

    rm lodash

--- src/components/ShopComp/ShopSideBar/index.jsx
+++ src/components/ShopComp/ShopSideBar/index.jsx
@@ -1,19 +1,16 @@
 import s from './ShopSideBar.module.css'
-import { convertArray } from '../../../utils'
-import { debounce } from 'lodash'
 
-
-export const ShopSideBar = ({searchHandler}) => {
-
-	
-
-    
+export const ShopSideBar = ({ searchHandler }) => {
 	return (
 		<section>
-			<input placeholder='Search here...' className={s.name} type='text' onChange={(e)=>searchHandler(e.target.value)} />
+			<input
+				placeholder='Search here...'
+				className={s.name}
+				type='text'
+				onChange={(e) => searchHandler(e.target.value)}
+			/>
 			{/* {console.log(shopProducts[0][1].name)} */}
-			<input type='range'/>
-			
+			<input type='range' />
 		</section>
 	)
 }
