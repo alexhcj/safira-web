3de92940b3bbfe7ceb4f003a33b24ac905120ec1
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-24 22:30:10 +0300

    add GoodToCart

---
+++ src/shared/components/GoodToCart/index.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import s from './goodtocart.module.scss'
+import {Text} from "../UI/Text/Text";
+import {Button} from "../../../components/UI/Buttons/Button/Button";
+import {Space} from "../../../components/Spacing/Space";
+
+export const GoodToCart = ({quantity}) => {
+    const [value, setValue] = useState(1)
+
+    const handleInput = (e) => {
+			const {value} = e.target
+			setValue(value)
+		}
+
+    return (
+			<>
+				<div className={s.box}>
+						<div className={s.input_box}>
+							<label className={s.input_label} htmlFor="quantity">Quantity</label>
+							<input className={s.input}
+									onInput={handleInput}
+									value={value}
+									type="number"
+									name="quantity"
+									min={1}
+									max={quantity}
+							/>
+						</div>
+						<Button className={s.btn_add} type='submit'>
+							<Text size="medium" color="white" weight="medium">Add to cart</Text>
+						</Button>
+				</div>
+				<Space size="s" />
+				<Button type="text">
+					{/* TODO: if product exist in wishlist => icon HEART. Hover on icon => popup (remove from wishlist) */}
+					<Text span>+ Add to WishList</Text>
+				</Button>
+			</>
+    )
+}
