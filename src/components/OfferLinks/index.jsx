e543dd0d593813456490cf67a033f90d3ba08c6d
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-24 22:25:49 +0300

    refactor OfferLinks with local data

--- src/components/OfferLinks/index.jsx
+++ src/components/OfferLinks/index.jsx
@@ -1,39 +1,29 @@
-import { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { offersAPI } from '../../api'
-import { ImageWithFallback } from '../../utils/ImageWithFallback'
+import Link1 from '../../assets/images/offer-links/fresh-vegetables/590x140.jpg'
+import Link2 from '../../assets/images/offer-links/natural-fresh-fruits/590x140.jpg'
+import {generateID} from "../../utils/IdGenerator";
 import s from './offerlinks.module.css'
 
+const links = [
+	{
+		id: generateID(),
+		img: Link1
+	},
+	{
+		id: generateID(),
+		img: Link2
+	}
+]
+
 export const OfferLinks = () => {
-	const [offers, setOffers] = useState([])
-	// const [isLoading, setIsLoading] = useState(false)
-
-	useEffect(() => {
-		const fetchData = async () => {
-			// setIsLoading(true)
-
-			try {
-				const data = await offersAPI.getOffers()
-				setOffers(data)
-			} catch (e) {
-				console.log(e)
-			}
-			// setIsLoading(false)
-		}
-
-		fetchData()
-	}, [])
-
 	return (
 		<div className={s.section}>
 			<div className='container'>
 				<div className={s.block}>
-					{offers.map((offer) => {
-						const { id, img } = offer
-
+					{links.map(({ id, img }) => {
 						return (
 							<NavLink className={s.link} to='/shop' key={id}>
-								<ImageWithFallback src={img} imgSize='offerlink' />
+								<img src={img} alt="Offer image" />
 							</NavLink>
 						)
 					})}
