038b3c5e6d4fac4595ab8fa81d26c065cc2ad3d7
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-24 21:45:09 +0300

    add Space

---
+++ src/components/Spacing/Space.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import cn from 'classnames';
+import s from './styles/spaces.module.scss';
+
+export const Space = ({
+  size,
+  space,
+  className,
+}) => (
+  <div
+    className={cn(
+        size && s[size],
+      className,
+    )}
+    style={{ height: `${space}px` }}
+  />
+);
