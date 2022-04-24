6d705425b7b24e13ec6be3d414505bdbead3e20e
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-24 21:44:19 +0300

    add Border separator comp

---
+++ src/components/Spacing/Border.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import cn from 'classnames';
+import s from './styles/border.module.sass';
+
+export const Border = ({
+  className,
+}) => (
+  <div
+    className={cn(
+      s.border,
+      className,
+    )}
+  />
+);
