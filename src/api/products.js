f8e29024d2a51f4615d1f8043439e0a1721c870e
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-30 14:34:31 +0300

    refactor products API

--- src/api/products.js
+++ src/api/products.js
@@ -3,11 +3,8 @@ import {API} from "./api";
 const BASE_URL = 'products'
 
 export const productsAPI = {
-    getAll({ search = '', sort = 'popularity', tag = '', order = 'desc', page = 1, limit = 10, category = '', name_ne = '' }) {
-        return API
-            .get(
-                `${BASE_URL}/list?name_like=${search}&_sort=${sort}&category_like=${category}&tags_like=${tag}&_order=${order}&_page=${page}&_limit=${limit}&name_ne=${name_ne}`
-            )
+    getAll(params) {
+        return API.get(`${BASE_URL}/list`, params)
     },
     getProductsByNewprice({ sort = 'newprice', order = 'desc', limit = 10, newprice_gte = 0 }) {
         return API
