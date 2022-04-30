d2e8d3266cf3d29e91bd778fb6b1743d60724565
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-30 14:42:23 +0300

    refactor ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { ImageWithFallback } from '../../utils/ImageWithFallback'
-import classNames from 'classnames/bind'
-import s from './productcard.module.css'
+import cn from "classnames";
+import {Price} from "../Price/Price";
 import {Tags} from "../../shared/components/UI/Tags/Tags";
 import {Hovermenu} from "../../shared/components/UI/Hovermenu/Hovermenu";
+import { ImageWithFallback } from '../../utils/ImageWithFallback'
+import s from './productcard.module.scss'
 
-let cx = classNames.bind(s)
-
+// sizes: xs | large
 export const ProductCard = ({ size, imgSize, product }) => {
 	const [menuToggle, setMenuToggle] = useState(false)
 	const [priceToggle, setPriceToggle] = useState(false)
 
-	const { slug, tags, img, name, category, price, newprice } = product
+	const { slug, tags, img, name, category, price } = product
 
 	const url = {
 		pathname: `/products/${slug}`,
@@ -21,8 +21,6 @@ export const ProductCard = ({ size, imgSize, product }) => {
 			category: category,
 		},
 	}
-
-	let productCN = cx('product', { large: size })
 
 	const handleMenuToggle = (e) => {
 		if (e.type === 'mouseenter') {
@@ -35,7 +33,7 @@ export const ProductCard = ({ size, imgSize, product }) => {
 	}
 
 	return (
-		<div onMouseEnter={handleMenuToggle} onMouseLeave={handleMenuToggle} className={productCN}>
+		<div onMouseEnter={handleMenuToggle} onMouseLeave={handleMenuToggle} className={cn(s.product, size && s[`product_${size}`])}>
 			<NavLink to={url}>
 				<ImageWithFallback className={s.img} src={img} alt={name} imgSize={imgSize} />
 			</NavLink>
@@ -46,16 +44,7 @@ export const ProductCard = ({ size, imgSize, product }) => {
 				<h4 className={s.category}>
 					<NavLink to='/shop'>{category}</NavLink>
 				</h4>
-				<div className={!priceToggle ? `${s.prices}` : `${s.prices} ${s.hide}`}>
-					{newprice === undefined ? (
-						<span className={s.price}>${price}</span>
-					) : (
-						<div>
-							<span className={s.priceDiscount}>${newprice}</span>
-							<span className={newprice ? `${s.price} ${s.discount}` : `${s.price}`}>${price}</span>
-						</div>
-					)}
-				</div>
+				<Price {...price} className={cn(s.prices, priceToggle && s.hide, size === 'xs' && s.flex_start)} />
 			</div>
 			{tags.length !== 0 && size === 'large' && <Tags tags={tags} />}
 			<Hovermenu menuToggle={menuToggle} size={size} />
@@ -65,5 +54,3 @@ export const ProductCard = ({ size, imgSize, product }) => {
 
 // TODO: menu last item crops with slider overflow
 // TODO: hover on slider => btns appear. slider paddings includes in triggered zone
-
-// TODO: fix hover on category
