bf51092d3f0b4ab68a7bbb0bc0de5eb490e4d8d0
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-30 14:41:24 +0300

    fix Tags

--- src/shared/components/UI/Tags/Tags.jsx
+++ src/shared/components/UI/Tags/Tags.jsx
@@ -4,7 +4,7 @@ export const Tags = ({tags}) => {
 	return (
 		<div className={s.tags}>
 			{tags.map((item, index) => (
-				<span className={s.tag} key={index}>{item.text}</span>
+				<span className={s.tag} key={index}>{item}</span>
 			))}
 		</div>
 	)
