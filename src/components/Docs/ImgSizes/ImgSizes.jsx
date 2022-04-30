16c7f7d3620ca2bd8e1ed7b51b52a8fc2e526f1b
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-30 14:48:08 +0300

    change ImgSize key value

--- src/components/Docs/ImgSizes/ImgSizes.jsx
+++ src/components/Docs/ImgSizes/ImgSizes.jsx
@@ -5,11 +5,11 @@ import s from './imgsizes.module.css'
 export const ImgSizes = () => {
     return (
         <div className={s.sizes}>
-            {sizeTypes.map((item, index) => {
+            {sizeTypes.map((item) => {
                 const { type, size } = item
 
                 return (
-                    <div className={s.item}>
+                    <div className={s.item} key={type}>
                         <span className={s.type}>{type}</span>
                         <span className={s.size}>{size}</span>
                     </div>
