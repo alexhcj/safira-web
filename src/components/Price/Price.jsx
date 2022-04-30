343cc9c73e64161ef1be34a3d578a5a2e1bd431f
alex_hcj <alexeykorolyov19@gmail.com>
2022-04-30 14:41:10 +0300

    refactor Price

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -1,11 +1,12 @@
-import s from './price.module.scss'
+import cn from "classnames";
 import {Text} from "../../shared/components/UI/Text/Text";
+import s from './price.module.scss'
 
-export const Price = ({price, discount_price}) => {
+export const Price = ({price, discount_price, className}) => {
 	return (
-		<div className={s.box}>
-			<Text span size="price" color="primary">${discount_price}</Text>
-			<Text span size="discount_price">${price}</Text>
+		<div className={cn(s.box, className)}>
+			{discount_price && <Text className={s.discount_price} span>${discount_price}</Text>}
+			<Text className={cn(s.price, discount_price && s.old)} span>${price}</Text>
 		</div>
 	)
 }
