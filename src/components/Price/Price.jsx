9fbc89dc169256e60d176e11ae19349f03fc0981
alex_hcj <alexeykorolyov19@gmail.com>
2022-05-01 19:03:44 +0300

    add Price type

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -2,9 +2,9 @@ import cn from "classnames";
 import {Text} from "../../shared/components/UI/Text/Text";
 import s from './price.module.scss'
 
-export const Price = ({price, discount_price, className}) => {
+export const Price = ({price, discount_price, type, className}) => {
 	return (
-		<div className={cn(s.box, className)}>
+		<div className={cn(s.box, type && s[type], className)}>
 			{discount_price && <Text className={s.discount_price} span>${discount_price}</Text>}
 			<Text className={cn(s.price, discount_price && s.old)} span>${price}</Text>
 		</div>
