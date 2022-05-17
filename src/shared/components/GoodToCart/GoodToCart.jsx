85b3f1b193d48b373e938a4a0563367912976eb8
alex_hcj <alexeykorolyov19@gmail.com>
2022-05-17 21:57:12 +0300

    refactor GoodToCart

--- src/shared/components/GoodToCart/GoodToCart.jsx
+++ src/shared/components/GoodToCart/GoodToCart.jsx
@@ -1,35 +1,37 @@
+import React from 'react';
 import { useState } from 'react'
-import s from './goodtocart.module.scss'
 import {Text} from "../UI/Text/Text";
 import {Button} from "../UI/Buttons/Button/Button";
 import {Space} from "../UI/Spacing/Space";
+import s from './goodtocart.module.scss'
 
-export const GoodToCart = ({quantity}) => {
+export const GoodToCart = ({maxQuantity, onClick, findProductCart}) => {
     const [value, setValue] = useState(1)
 
     const handleInput = (e) => {
 			const {value} = e.target
-			setValue(value)
+			setValue(+value)
 		}
 
     return (
 			<>
 				<div className={s.box}>
-						<div className={s.input_box}>
-							<label className={s.input_label} htmlFor="quantity">Quantity</label>
-							{/* TODO: add input max quantity validations */}
-							<input className={s.input}
-									onInput={handleInput}
-									value={value}
-									type="number"
-									name="quantity"
-									min={1}
-									max={quantity}
-							/>
-						</div>
-						<Button className={s.btn_add} type='submit' disabled={value > quantity}>
-							<Text size="medium" color="white" weight="medium">Add to cart</Text>
-						</Button>
+					{!findProductCart && <div className={s.input_box}>
+						<label className={s.input_label} htmlFor="quantity">Quantity</label>
+						{/* TODO: add input max quantity validations */}
+						<input className={s.input}
+							onInput={handleInput}
+							value={value}
+							type="number"
+							name="quantity"
+							min={1}
+							max={maxQuantity}
+						/>
+						</div>}
+					<Button className={s.btn_add} type='submit' disabled={value > maxQuantity || findProductCart} onClick={() => onClick(value)}>
+						<Text size="medium" color="white" weight="medium">{findProductCart ? 'Already in cart' : 'Add to cart'}</Text>
+						{/*<Text size="medium" color="white" weight="medium">{findProductCart ? 'Add more' : 'Add to cart'}</Text>*/}
+					</Button>
 				</div>
 				<Space size="s" />
 				<Button type="text">
