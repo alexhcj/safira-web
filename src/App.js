ae4a6006bcb2ccf56b59e2bb4d085a7e2458dfd7
alex_hcj <alexeykorolyov19@gmail.com>
2022-05-20 01:50:37 +0300

    refactor naming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import { Home, Shop, Blog, CartPage, Checkout, Product, Docs } from './pages'
+import { Home, Shop, Blog, CartPage, CheckoutPage, Product, Docs } from './pages'
 import {Header} from "./shared/components/common/Header/Header";
 import {Footer} from "./shared/components/common/Footer/Footer";
 import {Copyright} from "./components/Copyright/Copyright";
@@ -16,7 +16,7 @@ function App() {
 				<Route path='/blog'  element={<Blog />} />
 				<Route path='/blog/:id' element={<Blog />} />
 				<Route path='/cart' element={<CartPage />} />
-				<Route path='/checkout' element={<Checkout />} />
+				<Route path='/checkout' element={<CheckoutPage />} />
 				<Route path='/products/:slug' element={<Product />} />
 				<Route path='/docs' element={<Docs />} />
 			</Routes>
