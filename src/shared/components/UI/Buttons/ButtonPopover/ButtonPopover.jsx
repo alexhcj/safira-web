0e1cc109377d268142e4e561f7d98d91eb83add4
alex_hcj <alexeykorolyov19@gmail.com>
2022-05-21 23:50:21 +0300

    add ButtonPopover

---
+++ src/shared/components/UI/Buttons/ButtonPopover/ButtonPopover.jsx
@@ -0,0 +1,17 @@
+import cn from "classnames";
+import {Text} from "../../Text/Text";
+import s from './button-popover.module.scss'
+
+export const ButtonPopover = ({isHovered, text, className, onClick, onMouseEnter, onMouseLeave, children}) => {
+	return (
+		<button
+			className={cn(s.btn, isHovered && s.active, className)}
+			onClick={onClick}
+			onMouseEnter={onMouseEnter}
+			onMouseLeave={onMouseLeave}
+		>
+			{children}
+			<Text span className={s.text}>{text}</Text>
+		</button>
+	)
+}
