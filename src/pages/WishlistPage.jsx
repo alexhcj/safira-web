f733f96eb24e8a7124645548b1f7c0d32deb8cdc
alex_hcj <alexeykorolyov19@gmail.com>
2022-05-21 23:48:23 +0300

    add Wishlist

---
+++ src/pages/WishlistPage.jsx
@@ -0,0 +1,11 @@
+import {Breadcrumbs} from "../shared/components/UI/Breadcrumbs/Breadcrumbs";
+import {Wishlist} from "../modules/Wishlist/Wishlist";
+
+export const WishlistPage = () => {
+	return (
+		<div>
+			<Breadcrumbs />
+			<Wishlist />
+		</div>
+	)
+}
