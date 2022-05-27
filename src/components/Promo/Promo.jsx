d0e5bd97cd5f92a4089bbcb80e767a80563fa91a
alex_hcj <alexeykorolyov19@gmail.com>
2022-05-27 23:06:50 +0300

    refactor Sale to Promo

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button } from '../../shared/components/UI/Buttons/Button/Button'
 import { Text } from '../../shared/components/UI/Text/Text'
-import { Space } from '../../shared/components/UI/Spacing/Space'
-import SaleImg from '../../assets/images/sale/1920x440.jpg'
-import s from './sale.module.scss'
+import PromoImg from '../../assets/images/promo/1920x440.jpg'
+import s from './promo.module.scss'
 
-const sale = [
+const promo = [
 	{
 		id: 1,
 		title: 'Black Friday !',
-		saleInfo: 'Sale 50% OFF',
+		promoInfo: 'Sale 50% OFF',
 		description: 'all vegetable products',
-		img: SaleImg
+		img: PromoImg
 	}
 ]
 
-export const Sale = () => {
+// TODO: refactor to Banners
+export const Promo = () => {
 	const navigate = useNavigate()
-	const { title, img, saleInfo, description } = sale[0]
+	const { title, img, promoInfo, description } = promo[0]
 
 	const handleClick = (e) => {
 		e.preventDefault()
@@ -28,18 +28,17 @@ export const Sale = () => {
 
 	return (
 		<div className={s.section}>
-			<img className={s.img} src={img} alt={`${saleInfo} ${description}`} />
+			<img className={s.img} src={img} alt={`${promoInfo} ${description}`} />
 			<div className='container'>
 				<div className={s.block}>
 					<h4 className={s.title}>{title}</h4>
-					<h2 className={s.sale__info}>{saleInfo}</h2>
+					<h2 className={s.promo__info}>{promoInfo}</h2>
 					<p className={s.description}>{description}</p>
 					<Button className={s.btn} onClick={handleClick}>
 						<Text className={s.text} span color="white">Discover now</Text>
 					</Button>
 				</div>
 			</div>
-			<Space space={65} />
 		</div>
 	)
 }
