6b17ceebdda0be8a4df09e997a188b1ca9c1f1f6
alex_hcj <alexhcj@yandex.ru>
2023-07-25 14:07:59 +0300

    update: add ButtonCart

---
+++ src/shared/components/UI/Buttons/ButtonCart/ButtonCart.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import cn from 'classnames'
+import s from './button-cart.module.scss'
+
+export const ButtonCart = ({
+	type = 'button',
+	onClick,
+	className,
+	children
+}) => {
+	const [focus, setFocus] = useState(false)
+
+	const onFocus = () => {
+		setFocus(true)
+	}
+
+	const onBlur = () => {
+		setFocus(false)
+	}
+
+	return (
+		<button
+			type={type}
+			className={cn(s.btn, focus && s.focus, className)}
+			onClick={onClick}
+			onFocus={onFocus}
+			onBlur={onBlur}
+		>
+			{children}
+		</button>
+	)
+}
