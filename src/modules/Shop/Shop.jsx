32dfe2a33a04ad98259490029863ae4fd584b633
alex_hcj <alexhcj@yandex.ru>
2023-07-27 17:07:56 +0300

    refactor: structure, data flow

--- src/modules/Shop/Shop.jsx
+++ src/modules/Shop/Shop.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import { useProducts } from '../../hooks/useProducts'
 import { ShopList } from './ShopList/ShopList'
 import { ShopSort } from './ShopSort/ShopSort'
 import { Sidebar } from './Sidebar/Sidebar'
 import { Pagination } from '../../shared/components/Pagination/Pagination'
+import { ShopLayoutTypes } from './ShopListLayout/ShopListLayout'
 import s from './shop.module.scss'
 
+export const GridContext = createContext(null)
+
 export const Shop = () => {
-	const { data, meta, loading } = useProducts()
+	const [productsGrid, setProductsGrid] = useState(ShopLayoutTypes[0].type)
+	const { products, meta, loading } = useProducts()
 
 	return (
 		<>
 			<div className='container'>
 				<div className={s.wrapper}>
 					<div className={s.main}>
-						<ShopSort meta={meta} />
-						<ShopList products={data} />
+						<GridContext.Provider value={{ productsGrid, setProductsGrid }}>
+							<ShopSort meta={meta} />
+							<ShopList products={products} />
+						</GridContext.Provider>
 						<Pagination meta={meta} />
 					</div>
 					<div className={s.sidebar}>
-						<Sidebar isLoading={loading} />
+						<Sidebar isLoading={loading} meta={meta} />
 					</div>
 				</div>
 			</div>
