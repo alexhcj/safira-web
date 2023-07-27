40631f119eb37ff1c766a2075e8cbb57ffcd5ab6
alex_hcj <alexhcj@yandex.ru>
2023-07-27 17:05:18 +0300

    refactor: sidebar structure

---
+++ src/modules/Shop/Sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Search } from './Search/Search'
+import { FilterTitle } from '../../../shared/components/UI/Sidebar/FilterTitle/FilterTitle'
+import { PriceRange } from './PriceRange/PriceRange'
+import s from './sidebar.module.scss'
+
+export const Sidebar = ({ meta }) => {
+	return (
+		<aside className={s.aside}>
+			<div>
+				<FilterTitle text='Filter by name' />
+				<Search />
+			</div>
+			<div>
+				<FilterTitle text='Filter by price' />
+				<PriceRange meta={meta} />
+			</div>
+		</aside>
+	)
+}
