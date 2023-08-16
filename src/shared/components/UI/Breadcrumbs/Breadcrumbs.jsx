56c9d62bbe23373ef49dee640fa3dc16175733c3
alex_hcj <alexhcj@yandex.ru>
2023-08-16 20:01:22 +0300

    update: current & page breadcrumbs

--- src/shared/components/UI/Breadcrumbs/Breadcrumbs.jsx
+++ src/shared/components/UI/Breadcrumbs/Breadcrumbs.jsx
@@ -1,31 +1,40 @@
 import React from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
+import cn from 'classnames'
 import { Text } from '../Text/Text'
 import { Space } from '../Spacing/Space'
-import cn from 'classnames'
 import s from './breadcrumbs.module.scss'
 
 export const Breadcrumbs = () => {
-	let location = useLocation().pathname.slice(1) // breadcrumbs without /
-	const breadcrumbs = location.split('/')
+	let { pathname, state } = useLocation()
 
 	return (
 		<div className={s.section}>
 			<div className={s.content}>
-				<div className={s.box}>
-					<h3 className={s.page}>{breadcrumbs[0]}</h3>
-					<Space space={9} />
-					<div className={s.breadcrumbs}>
-						<div className={s.route}>
-							<NavLink to='/' className={cn(s.current, s.link)}>
-								Home
-							</NavLink>
-							<Text span>/</Text>
-						</div>
-						<div className={s.route}>
-							<Text className={s.current}>{breadcrumbs[0]}</Text>
-						</div>
+				<h3 className={s.page}>{pathname.slice(1)}</h3>
+				<Space space={9} />
+				<div className={s.breadcrumbs}>
+					<div>
+						<NavLink to='/' className={cn(s.current, s.link)}>
+							Home
+						</NavLink>
 					</div>
+					{pathname && !state.category && (
+						<>
+							<Text span>/ </Text>
+							<Text className={s.current} span>
+								{pathname.slice(1)}
+							</Text>
+						</>
+					)}
+					{state.category && (
+						<>
+							<Text span>/ </Text>
+							<Text className={s.current} span>
+								{state.category}
+							</Text>
+						</>
+					)}
 				</div>
 			</div>
 		</div>
