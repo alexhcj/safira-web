b195e30228279dbcc7278f62353094716a0a7c32
alex_hcj <alexhcj@yandex.ru>
2023-09-18 01:03:27 +0300

    add: helper func

--- src/utils/index.js
+++ src/utils/index.js
@@ -100,3 +100,20 @@ export const filterCharsOnly = (str) => {
 	// return str.match(/[A-Za-z]/) ? str : new Error('Validation error')
 	return str.match(/[A-Za-z]/)
 }
+
+export const shallowEqual = (obj1, obj2) => {
+	const keys1 = Object.keys(obj1)
+	const keys2 = Object.keys(obj2)
+
+	if (keys1.length !== keys2.length) {
+		return false
+	}
+
+	for (let key of keys1) {
+		if (obj1[key] !== obj2[key]) {
+			return false
+		}
+	}
+
+	return true
+}
