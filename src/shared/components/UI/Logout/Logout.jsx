0623ff067a49db6a400eb39f0f1c2f0c7f12627f
alex_hcj <alexhcj@yandex.ru>
2023-10-17 17:51:20 +0300

    update: font size & color

--- src/shared/components/UI/Logout/Logout.jsx
+++ src/shared/components/UI/Logout/Logout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import cn from 'classnames'
 import { useAuthContext } from '../../../../context/AuthContext'
 import { ReactComponent as LogoutSVG } from '../../../../assets/svg/logout.svg'
 import s from './logout.module.scss'
 
-export const Logout = () => {
+export const Logout = ({ className }) => {
 	const { logout } = useAuthContext()
 
 	return (
-		<div className={s.logout} onClick={() => logout()}>
+		<div className={cn(s.logout, className)} onClick={() => logout()}>
 			Logout
 			<LogoutSVG className={s.icon} />
 		</div>
