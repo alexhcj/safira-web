276f6d9139937f757c74fa8712ddaa554f1b9eca
alex_hcj <alexhcj@yandex.ru>
2024-07-21 02:28:16 +0300

    fix: params changing while navigate back

--- src/hooks/services/usePosts.jsx
+++ src/hooks/services/usePosts.jsx
@@ -3,21 +3,11 @@ import { useSearchParams } from 'react-router-dom'
 import { postsAPI } from '../../api/posts'
 
 export const usePosts = () => {
-	const [params, setParams] = useSearchParams()
+	const [params] = useSearchParams()
 	const [posts, setPosts] = useState([])
 	const [meta, setMeta] = useState({})
 	const [loading, setLoading] = useState(false)
 	const [error, setError] = useState(false)
-
-	useEffect(() => {
-		const defaultParams = {
-			offset: '0',
-			order: 'asc',
-			sort: 'createdAt',
-			limit: '2',
-		}
-		setParams(defaultParams)
-	}, [])
 
 	useEffect(() => {
 		const fetchData = async () => {
