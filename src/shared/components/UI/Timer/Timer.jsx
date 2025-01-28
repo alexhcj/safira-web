4a9f611196b07ed76e10796aa725ea0657075b2c
alex_hcj <alexhcj@yandex.ru>
2025-01-28 16:27:10 +0300

    add: date customizable props

--- src/shared/components/UI/Timer/Timer.jsx
+++ src/shared/components/UI/Timer/Timer.jsx
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from 'react'
 import cn from 'classnames'
 import s from './timer.module.scss'
 
-export const Timer = ({ date, className }) => {
-	const [days, setDays] = useState(0)
-	const [hours, setHours] = useState(0)
-	const [minutes, setMinutes] = useState(0)
+const TYPES = {
+	seconds: 1,
+	minutes: 2,
+	hours: 3,
+	days: 4,
+}
+
+export const Timer = ({ type, date, className }) => {
 	const [seconds, setSeconds] = useState(0)
+	const [minutes, setMinutes] = useState(0)
+	const [hours, setHours] = useState(0)
+	const [days, setDays] = useState(0)
+
 	const leading0 = (num) => {
 		return num < 10 ? '0' + num : num.toString()
 	}
 
 	const calculateTime = (date) => {
 		const time = Date.parse(date) - Date.parse(new Date())
+
 		if (time < 0) {
 			setDays(0)
 			setHours(0)
@@ -34,22 +43,30 @@ export const Timer = ({ date, className }) => {
 
 	return (
 		<div className={cn(s.timer, className)}>
-			<div className={s.item}>
-				<span className={s.number}>{leading0(days)}</span>
-				<span className={s.text}>Day</span>
-			</div>
-			<div className={s.item}>
-				<span className={s.number}>{leading0(hours)}</span>
-				<span className={s.text}>Hour</span>
-			</div>
-			<div className={s.item}>
-				<span className={s.number}>{leading0(minutes)}</span>
-				<span className={s.text}>Min</span>
-			</div>
-			<div className={s.item}>
-				<span className={s.number}>{leading0(seconds)}</span>
-				<span className={s.text}>Sec</span>
-			</div>
+			{TYPES[type] >= TYPES.days && (
+				<div className={s.item}>
+					<span className={s.number}>{leading0(days)}</span>
+					<span className={s.text}>Day</span>
+				</div>
+			)}
+			{TYPES[type] >= TYPES.hours && (
+				<div className={s.item}>
+					<span className={s.number}>{leading0(hours)}</span>
+					<span className={s.text}>Hour</span>
+				</div>
+			)}
+			{TYPES[type] >= TYPES.minutes && (
+				<div className={s.item}>
+					<span className={s.number}>{leading0(minutes)}</span>
+					<span className={s.text}>Min</span>
+				</div>
+			)}
+			{TYPES[type] >= TYPES.seconds && (
+				<div className={s.item}>
+					<span className={s.number}>{leading0(seconds)}</span>
+					<span className={s.text}>Sec</span>
+				</div>
+			)}
 		</div>
 	)
 }
