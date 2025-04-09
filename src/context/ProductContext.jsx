80d30ea78c3c34f7188c95e8758106c8cec84581
alex_hcj <alexhcj@yandex.ru>
2025-04-09 09:21:37 +0300

    fix: line separators

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react'
 
 const ProductModalContext = createContext([])
 export const useProductModalContext = () => useContext(ProductModalContext)
