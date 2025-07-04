b5d69c9e95382025c68832816156d8f71c7aeec9
alex_hcj <alexhcj@yandex.ru>
2025-07-04 13:27:19 +0300

    extend: types support

--- src/shared/components/UI/BlockNote/BlockNote.jsx
+++ src/shared/components/UI/BlockNote/BlockNote.jsx
@@ -2,6 +2,6 @@ import cn from 'classnames'
 
 import s from './block-note.module.scss'
 
-export const BlockNote = ({ children, className }) => {
-	return <div className={cn(s.box, className)}>{children}</div>
+export const BlockNote = ({ type, children, className }) => {
+	return <div className={cn(s.note, s[`note_${type}`], className)}>{children}</div>
 }
