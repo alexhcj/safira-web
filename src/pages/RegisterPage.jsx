130b785eb301919b9b64685aecb239fa3657c5ba
alex_hcj <alexhcj@yandex.ru>
2025-09-19 17:33:52 +0300

    add: redirect from register page when user is logged in

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,16 @@
+import { Navigate } from 'react-router-dom'
+
+import { useAuthContext } from '@context/AuthContext'
+
 import { RegisterForm } from '@shared/components/Form/AuthForms/RegisterForm'
 import { Breadcrumbs } from '@shared/components/UI/Breadcrumbs/Breadcrumbs'
 import { DefaultLayout } from '@shared/layouts/DefaultLayout/DefaultLayout'
 
 export const RegisterPage = () => {
+	const { user } = useAuthContext()
+
+	if (user) return <Navigate to='/' replace />
+
 	return (
 		<>
 			<Breadcrumbs />
