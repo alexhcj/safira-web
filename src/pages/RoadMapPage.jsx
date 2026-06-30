a419af744cc5283930aee3dcef59b8b5069b3bb0
alex_hcj <alexhcj@yandex.ru>
2026-06-30 15:25:37 +0300

    update: add RoadMap page adaptive styles

--- src/pages/RoadMapPage.jsx
+++ src/pages/RoadMapPage.jsx
@@ -2,14 +2,12 @@ import { RoadMap } from '@components/RoadMap/RoadMap'
 
 import { ScrollToTop } from '@shared/components/ScrollToTop/ScrollToTop'
 import { Breadcrumbs } from '@shared/components/UI/Breadcrumbs/Breadcrumbs'
-import { Space } from '@shared/components/UI/Spacing/Space'
 
 export const RoadMapPage = () => {
 	return (
 		<ScrollToTop>
 			<Breadcrumbs />
 			<RoadMap />
-			<Space size='l' />
 		</ScrollToTop>
 	)
 }
